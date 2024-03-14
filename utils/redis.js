b12a0693e73d8f000809f72852fa229bddd1bb58
MduduziSB <sibusisom.skhumkane@gmail.com>
2024-03-14 00:13:12 +0000

    Task 0

--- utils/redis.js
+++ utils/redis.js
@@ -4,7 +4,7 @@ const { promisify } = require('util');
 class RedisClient {
   constructor() {
     this.client = redis.createClient();
-    this.client.on('error', err => {
+    this.client.on('error', (err) => {
       console.error(`Redis client error: ${err}`);
     });
   }
@@ -15,17 +15,17 @@ class RedisClient {
 
   async get(key) {
     const getAsync = promisify(this.client.get).bind(this.client);
-    return await getAsync(key);
+    return getAsync(key);
   }
 
   async set(key, value, duration) {
     const setAsync = promisify(this.client.set).bind(this.client);
-    return await setAsync(key, value, 'EX', duration);
+    return setAsync(key, value, 'EX', duration);
   }
 
   async del(key) {
     const delAsync = promisify(this.client.del).bind(this.client);
-    return await delAsync(key);
+    return delAsync(key);
   }
 }
 
